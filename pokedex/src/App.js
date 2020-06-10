b59d547e23ec90d7261082e13034a40b0c489725
Esteban <emedina@creatonico.com>
2020-06-10 20:41:06 +0200

    update file

---
+++ pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <article>
+        <h1>Welcome to the Pokedex</h1>
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
+      </article>
+    </div>
+  );
+}
+
+export default App;
