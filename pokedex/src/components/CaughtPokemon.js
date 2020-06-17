518008d4692c1b899e3c38d85f38d092da222d48
Esteban <emedina@creatonico.com>
2020-06-17 20:00:18 +0200

    updated code

--- pokedex/src/components/CaughtPokemon.js
+++ pokedex/src/components/CaughtPokemon.js
@@ -1,4 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+
+class CaughtPokemon extends Component {
+  state = {
+    caughtPokemon: 0,
+  };
+
+  catch = () => {
+    this.setState((prevState) => {
+      return {
+        caughtPokemon: prevState.caughtPokemon + 1,
+      };
+    });
+  };
+
+  reset = () => {
+    this.setState(() => {
+      return {
+        caughtPokemon: 0,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Caught {this.state.caughtPokemon} Pokemon on </p>
+        {this.props.date}
+        <button onClick={this.catch}>Catch</button>
+
+        {this.state.caughtPokemon > 0 ? (
+          <ResetButton reset={this.reset} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const ResetButton = (props) => {
+  return <button onClick={props.reset}>Reset</button>;
+};
+
+export default CaughtPokemon;
+
+/*
+import React { Component } from "react";
+
 
 const CaughtPokemon = (props) => {
   return (
@@ -9,3 +55,4 @@ const CaughtPokemon = (props) => {
 };
 
 export default CaughtPokemon;
+*/
