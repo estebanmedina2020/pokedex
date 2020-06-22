f6ddb78d5cd5c921ccd4387f74b3fd5258eddfd9
Esteban <emedina@creatonico.com>
2020-06-22 22:25:34 +0200

    did exercise e

--- pokedex/src/components/BestPokemon.js
+++ pokedex/src/components/BestPokemon.js
@@ -1,89 +1,81 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+
+function BestPokemonFetcher() {
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1/")
+      .then((res) => res.json())
+
+      .then((data) => setbestPokemon(data));
+  }, []);
+
+  const [bestPokemon, setbestPokemon] = useState({});
+
+  return !bestPokemon ? null : <BestPokemon pokemon={bestPokemon} />;
+}
 
 class BestPokemon extends Component {
   state = {
     pokemonNames: [],
+
     isLoading: true,
   };
 
   componentDidMount() {
     fetch("https://pokeapi.co/api/v2/pokedex/1/")
       .then((res) => res.json())
+
       .then((data) => {
         const names = [
           data.pokemon_entries[0].pokemon_species.name,
+          data.pokemon_entries[1].pokemon_species.name,
+          data.pokemon_entries[2].pokemon_species.name,
           data.pokemon_entries[3].pokemon_species.name,
+          data.pokemon_entries[4].pokemon_species.name,
+          data.pokemon_entries[5].pokemon_species.name,
           data.pokemon_entries[6].pokemon_species.name,
         ];
-        setTimeout(() => {
-          this.setState({
-            pokemonNames: names,
-            isLoading: false,
-          });
-        }, 1000);
+
+        this.setState({
+          pokemonNames: names,
+
+          isLoading: false,
+        });
       });
   }
 
   render() {
-    return (
-      <div>
-        {this.state.isLoading ? (
-          <span>Loading...</span>
-        ) : (
-          <>
-            <h3>Names</h3>
-            <ul>
-              {this.state.pokemonNames.map((name, index) => {
-                return <li key={index}>{name}</li>;
-              })}
-            </ul>
-            <h3>Abilities</h3>
-            <ul>
-              {this.props.abilities.map((name, index) => {
-                return <li key={index}>{name}</li>;
-              })}
-            </ul>
-          </>
-        )}
-      </div>
-    );
+    if (this.state.isLoading) {
+      return <span>Loading ....</span>;
+    } else {
+      return (
+        <div>
+          <p>My favorite Pokemon is Pikachu</p>
+
+          {this.state.isLoading ? (
+            <span>Loading...</span>
+          ) : (
+            <>
+              <h3>Pokemon names</h3>
+
+              <ul>
+                {this.state.pokemonNames.map((name, index) => {
+                  return <li key={index}>{name}</li>;
+                })}
+              </ul>
+
+              <h3>Pokemon Abilities</h3>
+
+              <ul>
+                {this.props.Abilities.map((Abilitie, index) => {
+                  return <li key={index}>{Abilitie}</li>;
+                })}
+              </ul>
+            </>
+          )}
+        </div>
+      );
+    }
   }
 }
 
-export default BestPokemon;
-
-/*
-import React, { Component } from "react";
-
-class BestPokemon extends Component
-state = {
-    PokemonNames: [],
-  };
-
-  componentDidMount() {
-    fetch(`https://pokeapi.co/api/v2/pokedex/1/`);
-    .then(res => res.json())
-    .then(console.log(data.pokemon_entries[0].pokemon_species.name)
-  }
-
-
-  render() {
-  return (
-    <div>
-    <ul>
-      {props.abilities.map((abilities, index) => {
-        return (
-          <li>
-            {index}
-            {abilities}
-          </li>
-        );
-      })}
-    </ul>
-    </div>
-  );
-};
-}
-
-export default BestPokemon;
-*/
+export default BestPokemonFetcher;
